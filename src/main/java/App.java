12c2a888cafb6d0e7dc5bc1ada69e0fe4a7ee592
Courtney Phillips <courtney@epicodus.com>
2016-09-07 13:00:39 -0700

    Specify static file location in App.java file to allow Spark to locate image resources.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+    staticFileLocation("/public");
     get("/hello", (request, response) ->
        "<!DOCTYPE html>" +
         "<html>" +
