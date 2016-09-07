78d604e965c41d8e783c74f533796bfd00af9bd8
Courtney Phillips <courtney@epicodus.com>
2016-09-07 15:43:32 -0700

    Alter favorite_photos route in App.java to use favorite_photos.vtl template.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,22 +10,9 @@ public class App {
       return new ModelAndView(new HashMap(), "templates/hello.vtl");
     }, new VelocityTemplateEngine());
 
-   get("/favorite_photos", (request, response) ->
-    "<!DOCTYPE html>" +
-      "<html>" +
-      "<head>" +
-        "<title>Hello Friend!</title>" +
-        "<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-      "</head>" +
-      "<body>" +
-       "<h1>Favorite Traveling Photos</h1>" +
-          "<ul>" +
-            "<li><img src='/images/foggymountain.jpeg' alt='A photo of a mountain.'/></li>" +
-            "<li><img src='/images/rockycoast.jpeg' alt='A photo of a a rocky beach.'/></li>" +
-          "</ul>" +
-      "</body>" +
-      "</html>"
-    );
+    get("/favorite_photos", (request, response) -> {
+      return new ModelAndView(new HashMap(), "templates/favorite_photos.vtl");
+    }, new VelocityTemplateEngine());
 
   }
 }
