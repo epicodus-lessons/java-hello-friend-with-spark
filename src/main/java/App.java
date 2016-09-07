e7d793e6118754cb390c0fcb5a6d9ab7a566f803
Courtney Phillips <courtney@epicodus.com>
2016-09-07 15:45:13 -0700

    Fix spacing for easier readability.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,8 +4,10 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+
   public static void main(String[] args) {
     staticFileLocation("/public");
+    
     get("/", (request, response) -> {
       return new ModelAndView(new HashMap(), "templates/hello.vtl");
     }, new VelocityTemplateEngine());
