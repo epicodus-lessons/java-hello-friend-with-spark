0400a94419f67409519e511b13743b9a0172924f
Courtney Phillips <courtney@epicodus.com>
2016-09-09 13:40:41 -0700

    Add route for greeting_card.vtl template.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -28,5 +28,15 @@ public class App {
       return new ModelAndView(model, "templates/layout.vtl");
     }, new VelocityTemplateEngine());
 
+    get("/greeting_card", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String recipient = request.queryParams("recipient");
+      String sender = request.queryParams("sender");
+      model.put("recipient", recipient);
+      model.put("sender", sender);
+      model.put("template", "templates/greeting_card.vtl");
+      return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
+
   }
 }
